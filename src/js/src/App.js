25dbf3a641ae5a77ebbbe93d8ab1454e1bd051b9
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-07-24 21:37:10 +0530

    Students data displayed in table 🤩 using ant design

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Table } from "antd";
 
 function App() {
+  const [students, setStudents] = useState([]);
+  const columns = [
+    {
+      title: "Student ID",
+      dataIndex: "studentId",
+      key: "studentId",
+    },
+    {
+      title: "First Name",
+      dataIndex: "firstName",
+      key: "firstName",
+    },
+    {
+      title: "Last Name",
+      dataIndex: "lastName",
+      key: "lastName",
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+      key: "gender",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+  ];
+
+  useEffect(() => {
+    const getAllStudents = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/student");
+
+        if (!response.ok) {
+          console.log("Error");
+          return;
+        }
+
+        const studentList = await response.json();
+        setStudents(studentList);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllStudents();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      {!students && <h1>No Students found</h1>}
+      {students && <Table dataSource={students} columns={columns} />}
     </div>
   );
 }
