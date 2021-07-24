a2b4d56ced5b82c7b8633a83e572b1d84398dade
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-07-24 22:16:08 +0530

    Students data is rendered in a table with a loading spinner 😎😎

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,39 +1,56 @@
 import { useEffect, useState } from "react";
+import Container from "./components/Container";
 import "./App.css";
-import { Table } from "antd";
+import { Table, Avatar, Spin } from "antd";
+
+const columns = [
+  {
+    title: "",
+    key: "avatar",
+    render: (text, student) => {
+      return (
+        <Avatar size="large">
+          {`${student.firstName.charAt(0).toUpperCase()}${student.lastName
+            .charAt(0)
+            .toUpperCase()}`}
+        </Avatar>
+      );
+    },
+  },
+  {
+    title: "Student ID",
+    dataIndex: "studentId",
+    key: "studentId",
+  },
+  {
+    title: "First Name",
+    dataIndex: "firstName",
+    key: "firstName",
+  },
+  {
+    title: "Last Name",
+    dataIndex: "lastName",
+    key: "lastName",
+  },
+  {
+    title: "Gender",
+    dataIndex: "gender",
+    key: "gender",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+];
 
 function App() {
   const [students, setStudents] = useState([]);
-  const columns = [
-    {
-      title: "Student ID",
-      dataIndex: "studentId",
-      key: "studentId",
-    },
-    {
-      title: "First Name",
-      dataIndex: "firstName",
-      key: "firstName",
-    },
-    {
-      title: "Last Name",
-      dataIndex: "lastName",
-      key: "lastName",
-    },
-    {
-      title: "Gender",
-      dataIndex: "gender",
-      key: "gender",
-    },
-    {
-      title: "Email",
-      dataIndex: "email",
-      key: "email",
-    },
-  ];
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     const getAllStudents = async () => {
+      setIsFetching(true);
       try {
         const response = await fetch("http://localhost:8080/student");
 
@@ -47,15 +64,23 @@ function App() {
       } catch (error) {
         console.log(error);
       }
+      setIsFetching(false);
     };
     getAllStudents();
   }, []);
 
   return (
-    <div className="">
-      {!students && <h1>No Students found</h1>}
-      {students && <Table dataSource={students} columns={columns} />}
-    </div>
+    <Container>
+      {isFetching && <Spin />}
+      {students && (
+        <Table
+          rowKey="studentId"
+          pagination={false}
+          dataSource={students}
+          columns={columns}
+        />
+      )}
+    </Container>
   );
 }
 
